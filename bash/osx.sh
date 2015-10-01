1970cd4cf68ed1afc197ba807f0f51f27dbdce65
Rafa <rafa.vazsan@gmail.com>
2015-10-01 22:43:32 +0200

    Added git completion instructions

--- bash/osx.sh
+++ bash/osx.sh
@@ -11,3 +11,9 @@ export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
 alias ls='ls -GFh'
+
+# Bash (and git) completion
+## Requieres brew install bash-completion
+if [ -f `brew --prefix`/etc/bash_completion ]; then
+    . `brew --prefix`/etc/bash_completion
+fi
---
+++ git/autocompletion.md
@@ -0,0 +1,9 @@
+## OS X:
+```
+$ brew install bash-completion
+```
+
+bash_profile in this repo already has the configuration for bash-completion
+
+## Ubuntu:
+sudo apt-get install bash-completion
