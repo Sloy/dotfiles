ad5ba0f06847d470ede8cd3552becd9b9524282d
Rafa Vázquez <rafa.vazsan@gmail.com>
2015-10-22 20:16:30 +0200

    Fixes wrong alias for gradlew test

--- bash/alias/gradle.sh
+++ bash/alias/gradle.sh
@@ -1,6 +1,6 @@
 #Gradle wrapper
 alias gw='./gradlew'
 alias gbuild='./gradlew build'
-alias gbuild='./gradlew test'
+alias gtest='./gradlew test'
 alias gclean='./gradlew clean'
 alias gcleanbuild='./gradlew clean build'
