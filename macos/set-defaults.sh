912037593a9eaf0e51ca76e143408250ee3a056a
Rafa Vázquez <rafa.vazsan@gmail.com>
2016-08-04 13:42:35 +0200

    Disabled removal of Notification Center
    It doens't seem to work too well.

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -49,7 +49,7 @@ defaults write com.apple.helpviewer DevMode -bool true
 sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
 
 # Disable Notification Center and remove the menu bar icon
-launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
+#launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
 
 #Disable smart quotes as they’re annoying when typing code
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
