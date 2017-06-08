be1d18c3cb53c21c6393d3bc19ad5992f6f32f45
Rafa Vázquez <rafa.vazsan@gmail.com>
2017-06-08 20:12:21 +0200

    Add iTerm automatic config

--- install.sh
+++ install.sh
@@ -63,3 +63,9 @@ rm -rf fonts
 
 # Terminal theme
 open ./terminal/Chalk.terminal
+
+# iTerm2 (thanks http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/)
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
