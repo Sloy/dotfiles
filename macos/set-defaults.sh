2c8fc1a41bce4395e2353ec341c2e8d1ed72577f
Rafa Vázquez <rafa.vazsan@gmail.com>
2019-03-19 12:26:20 +0100

    Don't change LocalHostName, not working

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -16,7 +16,6 @@ sudo -v
 read -p "Enter your input here: " COMPUTER_NAME
 sudo scutil --set ComputerName "$COMPUTER_NAME"
 sudo scutil --set HostName "$COMPUTER_NAME"
-sudo scutil --set LocalHostName "$COMPUTER_NAME"
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$COMPUTER_NAME"
 
 
