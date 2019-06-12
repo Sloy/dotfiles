4e4f0ed591e4ca9b33df5db3a01867789f1fe05a
Rafa Vázquez <rafa.vazsan@gmail.com>
2019-06-12 18:03:22 +0200

    Add git pr command to push the current branch and open the Github page to create the Pull Request
    Thanks @padilo

---
+++ git/github-pull-request.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Credit to @padilo, many thanks!!
+
+url=$(git remote get-url origin)
+
+if [[ "$(echo $url | cut -d@ -f1)" != "git" ]]; then
+  echo "Unrecognized remote, can't open PR"
+  exit 1
+fi
+
+url_no_user=$(echo $url | cut -d@ -f2)
+host=$(echo $url_no_user | cut -d: -f1)
+project=$(echo $url_no_user | cut -d: -f2 | cut -d. -f1)
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+open "https://$host/$project/compare/$branch?expand=1"
