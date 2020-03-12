0f4fc184eca965076583cdd9604ba1ae8ea6b694
Rafa Vázquez <rafa.vazsan@gmail.com>
2020-03-12 10:22:02 +0100

    Add android tools/bin to path
    Includes tools like avd

--- zsh/.zsh/paths.zsh
+++ zsh/.zsh/paths.zsh
@@ -1,2 +1,2 @@
 export ANDROID_HOME=~/Library/Android/sdk
-export PATH=$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH
+export PATH=$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$PATH
