531ffba01a0b48eb17dbc4983898ffa34adfda2d
Rafa Vázquez <rafa.vazsan@gmail.com>
2023-06-30 11:20:38 +0200

    Create Gradle Home environment variable

--- zsh/.zsh/paths.zsh
+++ zsh/.zsh/paths.zsh
@@ -1,2 +1,3 @@
 export ANDROID_HOME=~/Library/Android/sdk
 export PATH=$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$PATH
+export GRADLE_USER_HOME=/Users/$(whoami)/.gradle
