8d3883fbbcc99f6ca4a801acc8eae1994a79f6a4
Rafa Vázquez <rafa.vazsan@gmail.com>
2023-06-30 11:20:06 +0200

    Homebrew config, don't remember why

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -35,3 +35,6 @@ then
   [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 fi
+
+# Homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
