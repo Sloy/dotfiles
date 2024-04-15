70f8772f25ae8a9aaaead860fef1d87583307854
Rafa Vázquez <rafa.vazsan@gmail.com>
2024-04-15 10:19:35 +0200

    Fix talkback command

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -92,9 +92,9 @@ alias deleteEmptyDirectories="find . -type d -empty -delete"
 
 function androidTalkBackToggle(){
   output=$(adb shell settings get secure enabled_accessibility_services)
-  if [[ "$output" == "com.android.talkback/com.google.android.marvin.talkback.TalkBackService" ]]; then
+  if [[ "$output" == "null" ]]; then
     adb shell settings put secure enabled_accessibility_services com.google.android.marvin.talkback/com.google.android.marvin.talkback.TalkBackService
   else
-    adb shell settings put secure enabled_accessibility_services com.android.talkback/com.google.android.marvin.talkback.TalkBackService
+    adb shell settings put secure enabled_accessibility_services null
   fi
 }
