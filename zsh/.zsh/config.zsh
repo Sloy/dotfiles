93b5e20a2eb1e04666a1189688b2e9c4de2f038a
Rafa Vázquez <rafa.vazsan@gmail.com>
2025-09-16 19:42:28 +0200

    Update NVM loading paths in zsh configuration

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -21,8 +21,8 @@ export LANG=en_US.UTF-8
 if [[ -a ~/.nvm ]]
 then
   export NVM_DIR="$HOME/.nvm"
-  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fi
 
 # Homebrew
