abe130de3bb36ceaa377700180f6e67eaeeef0fa
Rafa Vázquez <rafa.vazsan@gmail.com>
2026-03-13 11:41:15 +0100

    Remove oh-my-zsh, iTerm2, and dead steps from install.sh
    No longer using oh-my-zsh or iTerm2 (replaced by Warp). Removes
    oh-my-zsh, zsh-syntax-highlighting, powerlevel9k, Powerline fonts,
    Terminal.app theme, and iTerm2 config steps.

--- install.sh
+++ install.sh
@@ -38,37 +38,6 @@ createSymlink $DOTFILES_DIR/zsh/.zsh ~/.zsh
 # brew
 ./homebrew/install.sh
 
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# zsh syntax highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
 # zsh gradle completion
 git clone git://github.com/eriwen/gradle-completion ~/.zsh/gradle-completion
 
-# Download powerlevel9k oh-my-zsh theme if not exists, update otherwise
-[ -d ~/.oh-my-zsh/custom/themes/powerlevel9k ] && \
-  # exists
-  git -C ~/.oh-my-zsh/custom/themes/powerlevel9k pull || \
-  # doesn't exist
-  git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
-# Powerline fonts
-# clone
-git clone https://github.com/powerline/fonts.git
-# install
-cd fonts
-./install.sh
-# clean-up a bit
-cd ..
-rm -rf fonts
-
-# Terminal theme
-open ./terminal/Chalk.terminal
-
-# iTerm2 (thanks http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/)
-# Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
-# Tell iTerm2 to use the custom preferences in the directory
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
