e30d60cc4b3ad10be3064284977b470d07492435
Rafa Vázquez <rafa.vazsan@gmail.com>
2026-03-13 10:18:31 +0100

    Update git credential config and Android SDK paths
    Add osxkeychain credential helper and GitHub SSH URL rewrite to gitconfig. Update Android SDK PATH to use cmdline-tools instead of deprecated tools directories.

--- zsh/.zsh/paths.zsh
+++ zsh/.zsh/paths.zsh
@@ -1,3 +1,3 @@
 export ANDROID_HOME=~/Library/Android/sdk
-export PATH=$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$PATH
+export PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools:$PATH
 export GRADLE_USER_HOME=/Users/$(whoami)/.gradle
