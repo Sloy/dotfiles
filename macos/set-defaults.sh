f74803399e0e248fa1e55c8da5544b2e9b0b198c
Rafa Vázquez <rafa.vazsan@gmail.com>
2026-03-13 11:59:45 +0100

    Add interactive confirmation prompts to set-defaults.sh
    Each setting now asks for Y/n confirmation before applying, making the
    script suitable for both personal and work Mac setups.

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -9,6 +9,11 @@
 #
 # Run ./set-defaults.sh and you'll be good to go.
 
+confirm() {
+  read -r -p "$1 [Y/n] " response
+  [[ "$response" =~ ^([nN])$ ]] && return 1 || return 0
+}
+
 # Ask for the administrator password upfront
 sudo -v
 
@@ -20,171 +25,164 @@ if [[ ! (-z "$COMPUTER_NAME") ]]; then
 	sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$COMPUTER_NAME"
 fi
 
-# Use AirDrop over every interface. srsly this should be a default.
+confirm "Use AirDrop over every interface" && \
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
-# Show the ~/Library folder.
+confirm "Show the ~/Library folder" && \
 chflags nohidden ~/Library
 
-# Set the Finder prefs for showing a few different volumes on the Desktop.
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+if confirm "Show external drives and removable media on the Desktop"; then
+  defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+  defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+fi
 
-# Run the screensaver if we're in the bottom-left hot corner.
-defaults write com.apple.dock wvous-bl-corner -int 5
-defaults write com.apple.dock wvous-bl-modifier -int 0
+if confirm "Run screensaver on bottom-left hot corner"; then
+  defaults write com.apple.dock wvous-bl-corner -int 5
+  defaults write com.apple.dock wvous-bl-modifier -int 0
+fi
 
-# Disable rearrange of spaces on most recent use
+confirm "Disable rearrange of spaces based on most recent use" && \
 defaults write com.apple.dock mru-spaces -int 0
 
-# Expand save panel by default
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+if confirm "Expand save panel by default"; then
+  defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+  defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+fi
 
-# Save to disk (not to iCloud) by default
+confirm "Save to disk (not to iCloud) by default" && \
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
-# Automatically quit printer app once the print jobs complete
+confirm "Automatically quit printer app once the print jobs complete" && \
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
-# Disable the “Are you sure you want to open this application?” dialog
+confirm "Disable the 'Are you sure you want to open this application?' dialog" && \
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-# Remove duplicates in the “Open With” menu (also see `lscleanup` alias)
+confirm "Remove duplicates in the 'Open With' menu" && \
 /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
 
-# Set Help Viewer windows to non-floating mode
+confirm "Set Help Viewer windows to non-floating mode" && \
 defaults write com.apple.helpviewer DevMode -bool true
 
-# Reveal IP address, hostname, OS version, etc. when clicking the clock  in the login window
+confirm "Reveal IP address, hostname, OS version when clicking the clock in the login window" && \
 sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
 
-#Disable smart quotes as they’re annoying when typing code
-defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
-
-# Disable smart dashes as they’re annoying when typing code
-defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
-
+if confirm "Disable smart quotes and smart dashes"; then
+  defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+  defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+fi
 
 ###############################################################################
 # Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
 ###############################################################################
 
-# Trackpad: enable tap to click for this user and for the login screen
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+if confirm "Trackpad: enable tap to click"; then
+  defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+  defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+  defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+  defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+fi
 
-# Swipe between full-screen apps with 4 fingers
+confirm "Swipe between full-screen apps with 4 fingers" && \
 defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerHorizSwipeGesture -int 0
 
-# Mission control with 4 fingers
+confirm "Mission Control with 4 fingers" && \
 defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerVertSwipeGesture -int 0
 
-
-
-# Enable full keyboard access for all controls
-# (e.g. enable Tab in modal dialogs)
+confirm "Enable full keyboard access for all controls (e.g. Tab in modal dialogs)" && \
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-# Disable press-and-hold for keys in favor of key repeat.
+confirm "Disable press-and-hold for keys in favor of key repeat" && \
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-# Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain InitialKeyRepeat -int 15
-defaults write NSGlobalDomain KeyRepeat -float 2
-defaults write NSGlobalDomain com.apple.keyboard.fnState -int 1
+if confirm "Set a fast keyboard repeat rate"; then
+  defaults write NSGlobalDomain InitialKeyRepeat -int 15
+  defaults write NSGlobalDomain KeyRepeat -float 2
+  defaults write NSGlobalDomain com.apple.keyboard.fnState -int 1
+fi
 
-# Disable auto-correct
+confirm "Disable auto-correct" && \
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
-
 
 ###############################################################################
 # Screen                                                                      #
 ###############################################################################
 
-# Require password immediately after sleep or screen saver begins
-defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 0
+if confirm "Require password immediately after sleep or screen saver begins"; then
+  defaults write com.apple.screensaver askForPassword -int 1
+  defaults write com.apple.screensaver askForPasswordDelay -int 0
+fi
 
-# Change Screenshots location to Downloads folder
+confirm "Change screenshots location to Downloads folder" && \
 defaults write com.apple.screencapture location -string "~/Downloads"
 
 ###############################################################################
 # Finder                                                                      #
 ###############################################################################
 
-# Set Home as the default location for new Finder windows
-# For other paths, use `PfLo` and `file:///full/path/here/`
-defaults write com.apple.finder NewWindowTarget -string "PfLo"
-defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
+if confirm "Set Home as the default location for new Finder windows"; then
+  defaults write com.apple.finder NewWindowTarget -string "PfLo"
+  defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
+fi
 
-# Show icons for hard drives, servers, and removable media on the desktop
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+if confirm "Show icons for hard drives, servers, and removable media on the desktop"; then
+  defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+  defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+  defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+fi
 
-# Finder: show hidden files by default
-#defaults write com.apple.finder AppleShowAllFiles -bool true
-
-# Finder: show all filename extensions
+confirm "Finder: show all filename extensions" && \
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# Finder: show status bar
+confirm "Finder: show status bar" && \
 defaults write com.apple.finder ShowStatusBar -bool true
 
-# Finder: show path bar
+confirm "Finder: show path bar" && \
 defaults write com.apple.finder ShowPathbar -bool true
 
-# Display full POSIX path as Finder window title
+confirm "Display full POSIX path as Finder window title" && \
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
-# When performing a search, search the current folder by default
+confirm "When performing a search, search the current folder by default" && \
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
-# Disable the warning when changing a file extension
+confirm "Disable the warning when changing a file extension" && \
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-# Enable spring loading for directories
-defaults write NSGlobalDomain com.apple.springing.enabled -bool true
+if confirm "Enable spring loading for directories (with no delay)"; then
+  defaults write NSGlobalDomain com.apple.springing.enabled -bool true
+  defaults write NSGlobalDomain com.apple.springing.delay -float 0
+fi
 
-# Remove the spring loading delay for directories
-defaults write NSGlobalDomain com.apple.springing.delay -float 0
-
-# Avoid creating .DS_Store files on network volumes
+confirm "Avoid creating .DS_Store files on network volumes" && \
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-# Always open everything in Finder's list view.
-# Use list view in all Finder windows by default
-# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+confirm "Use list view in all Finder windows by default" && \
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
-# Expand the following File Info panes:
-# “General”, “Open with”, and “Sharing & Permissions”
+confirm "Expand General and Open With panes in File Info" && \
 defaults write com.apple.finder FXInfoPanesExpanded -dict General -bool true OpenWith -bool true
 
-# Enable snap-to-grid for icons on the desktop and in other icon views
-/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+if confirm "Enable snap-to-grid for icons on the desktop and in other icon views"; then
+  /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+  /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+  /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+fi
 
 ###############################################################################
-# Dock, Dashboard, and hot corners                                            #
+# Dock                                                                        #
 ###############################################################################
 
-# Enable highlight hover effect for the grid view of a stack (Dock)
+confirm "Enable highlight hover effect for the grid view of a stack (Dock)" && \
 defaults write com.apple.dock mouse-over-hilite-stack -bool true
 
-# Set the icon size of Dock items to 36 pixels
+confirm "Set the icon size of Dock items to 32 pixels" && \
 defaults write com.apple.dock tilesize -int 32
 
-# Wipe all (default) app icons from the Dock
-# This is only really useful when setting up a new Mac, or if you don’t use
-# the Dock to launch apps.
+confirm "Wipe all default app icons from the Dock" && \
 defaults write com.apple.dock persistent-apps -array
 
-# Don't show recently used applications in the Dock
+confirm "Don't show recently used applications in the Dock" && \
 defaults write com.Apple.Dock show-recents -bool false
 
 ###############################################################################
@@ -201,80 +199,63 @@ defaults write com.Apple.Dock show-recents -bool false
 #sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes"
 ## DOESN'T WORK IN MOJAVE
 
-# Change indexing order and disable some search results
-# Yosemite-specific search results (remove them if you are using macOS 10.9 or older):
-# 	MENU_DEFINITION
-# 	MENU_CONVERSION
-# 	MENU_EXPRESSION
-# 	MENU_SPOTLIGHT_SUGGESTIONS (send search queries to Apple)
-# 	MENU_WEBSEARCH             (send search queries to Apple)
-# 	MENU_OTHER
-defaults write com.apple.spotlight orderedItems -array \
-	'{"enabled" = 1;"name" = "APPLICATIONS";}' \
-	'{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
-	'{"enabled" = 1;"name" = "DIRECTORIES";}' \
-  '{"enabled" = 1;"name" = "MENU_CONVERSION";}' \
-  '{"enabled" = 1;"name" = "MENU_EXPRESSION";}' \
-  '{"enabled" = 1;"name" = "MENU_DEFINITION";}' \
-	'{"enabled" = 0;"name" = "PDF";}' \
-	'{"enabled" = 0;"name" = "FONTS";}' \
-	'{"enabled" = 0;"name" = "DOCUMENTS";}' \
-	'{"enabled" = 0;"name" = "MESSAGES";}' \
-	'{"enabled" = 0;"name" = "CONTACT";}' \
-	'{"enabled" = 0;"name" = "EVENT_TODO";}' \
-	'{"enabled" = 0;"name" = "IMAGES";}' \
-	'{"enabled" = 0;"name" = "BOOKMARKS";}' \
-	'{"enabled" = 0;"name" = "MUSIC";}' \
-	'{"enabled" = 0;"name" = "MOVIES";}' \
-	'{"enabled" = 0;"name" = "PRESENTATIONS";}' \
-	'{"enabled" = 0;"name" = "SPREADSHEETS";}' \
-	'{"enabled" = 0;"name" = "SOURCE";}' \
-	'{"enabled" = 0;"name" = "MENU_OTHER";}' \
-	'{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
-	'{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
+if confirm "Configure Spotlight indexing order (apps, system prefs, folders, calculators only)"; then
+  defaults write com.apple.spotlight orderedItems -array \
+    '{"enabled" = 1;"name" = "APPLICATIONS";}' \
+    '{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
+    '{"enabled" = 1;"name" = "DIRECTORIES";}' \
+    '{"enabled" = 1;"name" = "MENU_CONVERSION";}' \
+    '{"enabled" = 1;"name" = "MENU_EXPRESSION";}' \
+    '{"enabled" = 1;"name" = "MENU_DEFINITION";}' \
+    '{"enabled" = 0;"name" = "PDF";}' \
+    '{"enabled" = 0;"name" = "FONTS";}' \
+    '{"enabled" = 0;"name" = "DOCUMENTS";}' \
+    '{"enabled" = 0;"name" = "MESSAGES";}' \
+    '{"enabled" = 0;"name" = "CONTACT";}' \
+    '{"enabled" = 0;"name" = "EVENT_TODO";}' \
+    '{"enabled" = 0;"name" = "IMAGES";}' \
+    '{"enabled" = 0;"name" = "BOOKMARKS";}' \
+    '{"enabled" = 0;"name" = "MUSIC";}' \
+    '{"enabled" = 0;"name" = "MOVIES";}' \
+    '{"enabled" = 0;"name" = "PRESENTATIONS";}' \
+    '{"enabled" = 0;"name" = "SPREADSHEETS";}' \
+    '{"enabled" = 0;"name" = "SOURCE";}' \
+    '{"enabled" = 0;"name" = "MENU_OTHER";}' \
+    '{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
+    '{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
+  killall mds > /dev/null 2>&1
+  sudo mdutil -i on / > /dev/null
+  sudo mdutil -E / > /dev/null
+fi
 
-# Load new settings before rebuilding the index
-killall mds > /dev/null 2>&1
-# Make sure indexing is enabled for the main volume
-sudo mdutil -i on / > /dev/null
-# Rebuild the index from scratch
-sudo mdutil -E / > /dev/null
-
-# Enable Secure Keyboard Entry in Terminal.app
-# See: https://security.stackexchange.com/a/47786/8918
+confirm "Enable Secure Keyboard Entry in Terminal.app" && \
 defaults write com.apple.terminal SecureKeyboardEntry -bool true
 
 ###############################################################################
 # Time Machine                                                                #
 ###############################################################################
 
-# Prevent Time Machine from prompting to use new hard drives as backup volume
+confirm "Prevent Time Machine from prompting to use new hard drives as backup volume" && \
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
 ###############################################################################
 # Activity Monitor                                                            #
 ###############################################################################
 
-# Show the main window when launching Activity Monitor
-defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
-
-# Visualize CPU usage in the Activity Monitor Dock icon
-defaults write com.apple.ActivityMonitor IconType -int 5
-
-# Show all processes in Activity Monitor
-defaults write com.apple.ActivityMonitor ShowCategory -int 0
-
-# Sort Activity Monitor results by CPU usage
-defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
-defaults write com.apple.ActivityMonitor SortDirection -int 0
+if confirm "Configure Activity Monitor (show all processes, sort by CPU)"; then
+  defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
+  defaults write com.apple.ActivityMonitor IconType -int 5
+  defaults write com.apple.ActivityMonitor ShowCategory -int 0
+  defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+  defaults write com.apple.ActivityMonitor SortDirection -int 0
+fi
 
 ###############################################################################
 # Photos                                                                      #
 ###############################################################################
 
-# Prevent Photos from opening automatically when devices are plugged in
+confirm "Prevent Photos from opening automatically when devices are plugged in" && \
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
-
 
 for app in "Address Book" "Calendar" "Contacts" "Dock" "Finder" "Mail" "Safari" "SystemUIServer" "iCal"; do
   killall "${app}" &> /dev/null
