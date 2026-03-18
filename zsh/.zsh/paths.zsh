6c05712bba6b904064baf441484e0637e4ff357e
Rafa Vázquez <rafa.vazsan@gmail.com>
2026-03-18 13:16:24 +0100

    Add maestro to the path

--- zsh/.zsh/paths.zsh
+++ zsh/.zsh/paths.zsh
@@ -1,3 +1,4 @@
 export ANDROID_HOME=~/Library/Android/sdk
 export PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools:$PATH
 export GRADLE_USER_HOME=/Users/$(whoami)/.gradle
+export PATH=$PATH:$HOME/.maestro/bin
