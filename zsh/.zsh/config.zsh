da19fc420c059c0f790dee78203004b28c12d0d3
Rafa Vázquez <rafa.vazsan@gmail.com>
2026-03-18 13:10:01 +0100

    Fix nvm path

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -17,9 +17,9 @@ export LANG=en_US.UTF-8
 # Nvm
 if [[ -a ~/.nvm ]]
 then
-   export NVM_DIR="$HOME/.nvm"
-  [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 fi
 
 # Homebrew on MacOS ARM
